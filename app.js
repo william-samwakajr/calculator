37658a1e2a99012d0b62a28426a9c0881bb2d704
william-samwakajr <williamsamwaka05@gmail.com>
2023-08-19 20:13:53 +0200

    test version 0
    trying to add a new algorithm to fix the bugs my previous one had

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//de clearing all the button variables
+//declaring all the button variables
 let numInput1 = document.querySelector('.btn1');
 let numInput2 = document.querySelector('.btn2');
 let numInput3 = document.querySelector('.btn3');
@@ -17,74 +17,46 @@ let div = document.querySelector('.btn÷');
 let mul = document.querySelector('.btnm');
 let eq = document.querySelector('.btne');
 let solution = document.querySelector('.scrn');
-// input field variable
-let inputField2 = "";
-//adding  functionality to the buttons
 
-clear.addEventListener('click', function() {
-    document.querySelector('.scrn').reset();
+// input variables 
+let firstInput = '';
+let SecondInput = '';
+let operator = '';
 
-});
-del.addEventListener('click', function() {
+// operator functions 
+let addition = function() {
+    let ans = firstInput + SecondInput;
+    return ans;
+};
+let subtract = function() {
+    let ans = firstInput - SecondInput;
+    return ans;
+};
+let divide = function() {
+    let ans = firstInput / SecondInput;
+    return ans;
+};
+let multiply = function() {
+    let ans = firstInput * SecondInput;
+    return ans;
+};
 
-});
-add.addEventListener('click', function() {
-    inputField2 += '+';
-    solution.innerHTML = inputField2;
-});
-sub.addEventListener('click', function() {
-    inputField2 += '-';
-    solution.innerHTML = inputField2;
-});
-div.addEventListener('click', function() {
-    inputField2 += '/';
-    solution.innerHTML = inputField2;
-});
-mul.addEventListener('click', function() {
-    inputField2 += '*';
-    solution.innerHTML = inputField2;
-});
-eq.addEventListener('click', function() {
-    inputField2 = eval(inputField2);
-    solution.innerText = inputField2;
-});
-numInput1.addEventListener('click', function() {
-    inputField2 += '1';
-    solution.innerHTML = parseInt(inputField2);
-});
-numInput2.addEventListener('click', function() {
-    inputField2 += '2';
-    solution.innerHTML = parseInt(inputField2);
-});
-numInput3.addEventListener('click', function() {
-    inputField2 += '3';
-    solution.innerHTML = parseInt(inputField2);
-});
-numInput4.addEventListener('click', function() {
-    inputField2 += '4';
-    solution.innerHTML = parseInt(inputField2);
-});
-numInput5.addEventListener('click', function() {
-    inputField2 += '5';
-    solution.innerHTML = parseInt(inputField2);
-});
-numInput6.addEventListener('click', function() {
-    inputField2 += '6';
-    solution.innerHTML = parseInt(inputField2);
-});
-numInput7.addEventListener('click', function() {
-    inputField2 += '7';
-    solution.innerHTML = parseInt(inputField2);
-});
-numInput8.addEventListener('click', function() {
-    inputField2 += '8';
-    solution.innerHTML = parseInt(inputField2);
-});
-numInput9.addEventListener('click', function() {
-    inputField2 += '9';
-    solution.innerHTML = parseInt(inputField2);
-});
-numInput0.addEventListener('click', function() {
-    inputField2 += '0';
-    solution.innerHTML = parseInt(inputField2);
+// function that handles the calculations 
+function operate() {
+    operator = '+';
+    let finalAns = '';
+    if (operator == '+') {
+        finalAns = addition();
+    } else if (operator == '-') {
+        finalAns = subtract();
+    } else if (operator == '×') {
+        finalAns = multiply();
+    } else if (operator == '÷') {
+        finalAns = divide();
+    }
+    return finalAns
+}
+$("btn1").click(function() {
+    var fired_button = $("button").val();
+    alert(fired_button);
 });
